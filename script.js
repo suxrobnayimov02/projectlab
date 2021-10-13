9d0921231aaa0312816ff1e3015932a9bd1a87a0
xushnudbek20 <xushnud.khan@gmail.com>
2021-10-13 14:39:59 +0500

    new page

--- script.js
+++ script.js
@@ -19,3 +19,8 @@ window.addEventListener('scroll' , () =>{
         });
     });
 });
+
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+}
